8fa6f0a23e2573a7f16c6fbbd212da058a56bf8c
Maryana <renvema@bigmir.net>
2019-06-21 14:42:44 +0300

    less 13 commit1

---
+++ src/task/thirteenth/MinMax.java
@@ -0,0 +1,24 @@
+package task.thirteenth;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MinMax {
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+        List<T> list = stream
+                .sorted(order)
+                .collect(Collectors.toList());
+        if (list.isEmpty()) {
+            minMaxConsumer.accept(null, null);
+        } else {
+            minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
+        }
+    }
+}
